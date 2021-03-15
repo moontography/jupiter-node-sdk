9d419a3b8aa01d727b841b4e61518824a868fe0f
Lance Whatley <whatl3y@gmail.com>
2021-03-15 09:02:41 -0400

    export generatePassphrase

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Encryption from './Encryption'
 import JupiterClient from './JupiterClient'
+import { generatePassphrase } from './utils'
 
 export default JupiterClient
-export { Encryption }
+export { Encryption, generatePassphrase }
